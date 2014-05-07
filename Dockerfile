b633a983c848d2dc351ed409d5868361a48555f0
Anders Sveen <anders@f12>
2014-05-07 21:49:08 +0200

    Testing run of transmission-cli

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ MAINTAINER Anders Sveen <anders@f12.no>
 
 RUN apt-get -q update && apt-get install -qy --force-yes unzip openvpn software-properties-common && apt-get -qy --force-yes upgrade
 
-RUN add-apt-repository ppa:transmissionbt/ppa
-RUN apt-get -q update && apt-get install -qy --force-yes transmission-daemon
+#RUN add-apt-repository ppa:transmissionbt/ppa
+#RUN apt-get -q update && apt-get install -qy --force-yes transmission-daemon
+
+RUN apt-get install -qy --force-yes transmission-cli
 
 ADD run.sh /run.sh
+ADD shutdown.sh /shutdown.sh
 
 VOLUME [ "/config" ]
 
--- run.sh
+++ run.sh
@@ -5,3 +5,15 @@
 
 echo "Initiating OpenVPN connection"
 openvpn --writepid /var/run/openvpn.pid --daemon --status /var/run/openvpn.status 10 --cd /config --config /config/server.ovpn
+
+echo "Starting Transmission download"
+
+transmission-cli -f /shutdown.sh magnet:?xt=urn:btih:9bc21e6a74f20f9fedd55419417753838d4036cd&dn=Elementary+S02E22+HDTV+x264-LOL+%5Beztv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker-ccc.de%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337
+
+sleep 5s
+
+while [ $(pgrep transmission) ]; do
+	sleep 5s
+done
+
+echo "Transmission died, cleaning up!"
