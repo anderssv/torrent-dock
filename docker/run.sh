3cbcc41fe2eba262d51d66a9048ccf58f5d34939
Anders Sveen <anders@f12>
2014-07-17 16:15:49 +0200

    Changed server and error checking

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,11 @@
 [ -d /dev/net ] || mkdir -p /dev/net
 [ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200
 
+if [[ ! -e /config/vpn ]]; then
+	echo "ERROR: Could not find vpn config directory at '/config/vpn' . Something might be wrong with the Docker mount of paths?"
+	exit 1
+fi
+
 echo "Initiating OpenVPN connection"
 openvpn --writepid /var/run/openvpn.pid --daemon --status /var/run/openvpn.status 10 --cd /config/vpn --config /config/vpn/server.ovpn
 
