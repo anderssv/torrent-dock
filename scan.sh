731301898451aaad4b354d724ee93d11ca5fa44c
Anders Sveen <anders@f12>
2014-12-28 11:24:17 +0100

    Added multi-download to scan

--- scan.sh
+++ scan.sh
@@ -2,9 +2,17 @@
 
 links="$(curl $(cat rss.url) | grep -o '<link>[^<]*' | cut -c 7-)"
 
+tempfile=$(mktemp --tmpdir torrent_scan.XXXXXX)
+
 for link in $links; do
   if [[ ! $(grep "$link" downloaded.txt) ]]; then
-  	./start.sh "$link" "$HOME/Downloads/torrent_auto"
-  	echo $link >> downloaded.txt
+  	echo $link >> $tempfile
   fi
 done
+
+if [[ $(cat $tempfile | wc -l) -gt 0 ]]; then
+	./start.sh $tempfile "$HOME/Downloads/torrent_auto"
+fi
+
+cat $tempfile >> downloaded.txt
+rm $tempfile
