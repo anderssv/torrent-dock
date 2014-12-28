65f41feb434175657924ee671f2a8f5c71823941
Anders Sveen <anders@f12>
2014-12-28 10:54:35 +0100

    Better handling of adding torrents later

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,24 @@ function webAddress() {
 	echo "http://$(ifconfig | grep 'inet addr' | grep 172.17 | cut -c21- | cut -f1 -d ' '):9091"
 }
 
+function addTorrents() {
+	local torrent_file="$1"
+
+	echo "Adding torrents"
+	for torrent in $(cat $torrent_file); do
+		transmission-remote -a $torrent
+	done
+
+	if [[ -e /config/transmission/trackers.conf ]]; then
+		for torrent_id in $(transmission-remote -l | tr -s ' ' | grep -o '^[ ]*[0-9]\{1,3\}'); do
+			for tracker in $(cat /config/transmission/trackers.conf | grep -v \#); do
+				transmission-remote -t $torrent_id -td $tracker
+				echo "Added $tracker as tracker to torrent $ctr"
+			done
+		done
+	fi	
+}
+
 [ -d /dev/net ] || mkdir -p /dev/net
 [ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200
 
@@ -37,22 +55,7 @@ echo "Transmission started. Web available at: $(webAddress)"
 
 sleep 5s
 
-torrent_file="/download/torrent_download.list"
-num_torrents=$(cat $torrent_file | sed '/^$/d' | wc -l)
-
-echo "Adding $num_torrents torrents"
-for torrent in $(cat /download/torrent_download.list); do
-	transmission-remote -a $torrent
-done
-
-if [[ -e /config/transmission/trackers.conf ]]; then
-	for ((ctr=1; ctr <= $num_torrents; ctr++ )); do
-		for tracker in $(cat /config/transmission/trackers.conf | grep -v \#); do
-			transmission-remote -t $ctr -td $tracker
-			echo "Added $tracker as tracker to torrent $ctr"
-		done
-	done
-fi
+addTorrents "/download/torrent_download.list"
 
 sleep 5s
 
@@ -65,6 +68,12 @@ while [[ $(pgrep transmission) ]]; do
 	echo ""
 	transmission-remote -l
 	sleep 1s
+
+	new_torrents_file="/download/new_torrents.list"
+	if [[ -e $new_torrents_file ]]; then
+		addTorrents $new_torrents_file
+		rm $new_torrents_file
+	fi
 done
 
 echo "Transmission died, cleaning up!"
