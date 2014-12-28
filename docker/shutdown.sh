f5a2748a793f4f263ad289a790c5753afb880a0c
Anders Sveen <anders@f12>
2014-12-28 09:38:38 +0100

    Changed shutdown to support multiple torrents

--- docker/shutdown.sh
+++ docker/shutdown.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-pgrep transmission | xargs kill
-pgrep openvpn | xargs kill
+transmission-remote -t $TR_TORRENT_ID -r
+
+if [[ ! $(transmission-remote -l | wc -l) -gt 2 ]]; then
+	pgrep transmission | xargs kill
+fi
