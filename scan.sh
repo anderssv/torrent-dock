f6ea5574457d9e287b17f5ff82256550a88d8da9
Anders Sveen <anders@f12>
2015-01-10 13:58:22 +0100

    Added extra bootstrap

--- scan.sh
+++ scan.sh
@@ -2,9 +2,14 @@
 
 links="$(curl $(cat rss.url) | grep -o '<link>[^<]*' | cut -c 7-)"
 tempfile=$(mktemp --tmpdir torrent_scan.XXXXXX)
+downloaded_file="downloaded.txt"
+
+if [[ ! -e $downloaded_file ]]; then
+	touch $downloaded_file
+fi
 
 for link in $links; do
-  if [[ ! $(grep "$link" downloaded.txt) ]]; then
+  if [[ ! $(grep "$link" $downloaded_file) ]]; then
   	echo $link >> $tempfile
   fi
 done
@@ -13,5 +18,5 @@ if [[ $(cat $tempfile | wc -l) -gt 0 ]]; then
 	./start.sh $tempfile "$HOME/Downloads/torrent_auto"
 fi
 
-cat $tempfile >> downloaded.txt
+cat $tempfile >> $downloaded_file
 rm $tempfile
